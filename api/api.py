363a9fb7dda56db5056e7a3e63155040093f7597
sid2021 <wolanski.jakub@gmail.com>
2020-11-24 09:51:49 +0100

    added frontend

--- api/api.py
+++ api/api.py
@@ -1,4 +1,3 @@
-import time
 import datetime
 from flask import Flask
 
@@ -7,7 +6,4 @@ app = Flask(__name__)
 @app.route("/time")
 def get_current_time():
     # No need to jsonify the return because Flask does it for us
-    return {'time.time()': time.time(),
-            'datetime.date.today()': datetime.date.today(),
-            'datetime.datetime.utcnow()': datetime.datetime.utcnow(),
-            'datetime.date.fromtimestamp(time.time())': datetime.date.fromtimestamp(time.time())}
+    return {'time': datetime.datetime.utcnow()}
